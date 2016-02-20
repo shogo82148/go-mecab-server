8902f67c3477aaa650341b3300fc768f9ffe0217
Ichinose Shogo <shogo82148@gmail.com>
2016-02-21 04:33:08 +0900

    graceful restart

--- main.go
+++ main.go
@@ -3,20 +3,52 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"syscall"
 
+	"github.com/lestrrat/go-server-starter/listener"
+	"github.com/shogo82148/go-gracedown"
 	"github.com/shogo82148/go-mecab"
 )
 
 var model mecab.Model
 
 func main() {
+	signal_chan := make(chan os.Signal)
+	signal.Notify(signal_chan, syscall.SIGTERM)
+	go func() {
+		for {
+			s := <-signal_chan
+			if s == syscall.SIGTERM {
+				gracedown.Close()
+			}
+		}
+	}()
+
+	listeners, err := listener.ListenAll()
+	if err != nil && err != listener.ErrNoListeningTarget {
+		panic(err)
+	}
+	var l net.Listener
+	if err == listener.ErrNoListeningTarget {
+		// Fallback if not running under Server::Starter
+		l, err = net.Listen("tcp", ":8080")
+		if err != nil {
+			panic("Failed to listen to port 8080")
+		}
+	} else {
+		l = listeners[0]
+	}
+
 	var err error
 	model, err = mecab.NewModel(map[string]string{})
 	if err != nil {
 		panic(err)
 	}
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handler)
+	gracedown.Serve(l, mux)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
