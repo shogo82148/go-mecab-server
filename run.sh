2bad6c1c60518bcdb79161e0e3eee234f4ef8997
Ichinose Shogo <shogo82148@gmail.com>
2016-02-21 04:27:18 +0900

    add run scripts

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -ue
+
+ROOT=$(cd $(dirname $0);pwd)
+
+cd $ROOT
+exec start_server --port=9001 --kill-old-delay=60 --pid-file=app.pid -- $ROOT/start-worker.sh
---
+++ start-worker.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+set -ue
+
+echo "[$$] start"
+
+ROOT=$(cd $(dirname $0);pwd)
+CONTAINER_DIR=$ROOT/containers/$$
+mkdir -p $CONTAINER_DIR
+
+_cleanup_dir() {
+    echo "[$$] clean up"
+    rm -rf $CONTAINER_DIR
+    echo "[$$] finish"
+}
+
+trap _cleanup_dir EXIT
+
+# export image
+CONTAINER_ID=$(docker create mecab:latest)
+echo "[$$] export container:$CONTAINER_ID into $CONTAINER_DIR"
+docker export $CONTAINER_ID | tar x -C $CONTAINER_DIR
+docker rm $CONTAINER_ID
+
+# start daemon
+droot run --root $CONTAINER_DIR /go/bin/app &
+CHILD=$!
+
+# Forward SIGTERM and SIGHUP to child
+# http://unix.stackexchange.com/questions/146756/forward-sigterm-to-child-in-bash
+_term() {
+  echo "[$$] Caught SIGTERM signal!"
+  kill -TERM "$CHILD" 2>/dev/null
+}
+
+_hup() {
+    echo "[$$] Caught SIGHUP signal!"
+    kill -HUP "$CHILD" 2>/dev/null
+}
+
+_int() {
+    echo "[$$] Caught SIGINT signal!"
+    kill -INT "$CHILD" 2>/dev/null
+}
+
+trap _term SIGTERM
+trap _hup SIGHUP
+trap _int SIGINT
+
+_cleanup_child() {
+    echo "[$$] child $CHILD has finished. clean up"
+    droot rm --root $CONTAINER_DIR
+    echo "[$$] finish"
+}
+
+trap _cleanup_child EXIT
+
+echo "[$$] child $CHILD has started."
+wait "$CHILD"
