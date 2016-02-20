cf068113e7c5900959e1ff389d4943b791ea6d29
Ichinose Shogo <shogo82148@gmail.com>
2016-02-21 06:06:15 +0900

    interval option

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ set -ue
 ROOT=$(cd $(dirname $0);pwd)
 
 cd $ROOT
-exec start_server --port=9001 --kill-old-delay=60 --pid-file=app.pid -- $ROOT/start-worker.sh
+exec start_server --port=9001 --interval=30 --kill-old-delay=60 --pid-file=app.pid -- $ROOT/start-worker.sh
